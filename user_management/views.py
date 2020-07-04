822a18d9fca563909cecd79ed717a88900b2b2c5
Anupam Nirwan <anupam.nirwan@magicpin.in>
2020-07-04 23:55:11 +0530

    Update Import Pattern
    Line 1 -> Python imports
    Line 2 -> Django Imports
    Line 3 -> Rest and third party library imports
    Line 4 -> Other Apps Import
    Line 5 -> Same App Imports
    Single Line Space after each import group

--- user_management/views.py
+++ user_management/views.py
@@ -1,39 +1,43 @@
-from django.http import HttpResponse, JsonResponse
+import json
+import requests
+
 # from django.contrib.auth import authenticate, login
+from django.shortcuts import render
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .serializers import SignUpStudent_Serializer,SignUpEducator_Serializer
-from .models import User
+
 from rest_framework import status
 from rest_framework.decorators import api_view
-import requests
-import json
-from . import user_management_utils as user_utils
-from . import constants
+
+from . import (
+    constants,
+    serializers,
+    models,
+    user_management_utils as utils
+)
 
 
-def index(request):
-    # test view for experiments
-    return HttpResponse("hello world")
+def home(request):
+    # move to separate app if needed
+    response = render(request, 'home.html', {})
+    return response
 
+###! Move the following to api.py
 
 @csrf_exempt
 @api_view(['POST'])
 def signup_student(request):
-
-    signup_student_serializer = SignUpStudent_Serializer(data=request.data)
-        
+    signup_student_serializer = serializers.SignUpStudent_Serializer(data=request.data)
     if signup_student_serializer.is_valid():
         signup_student_serializer.save()
         return JsonResponse(signup_student_serializer.data,status=status.HTTP_201_CREATED)
     else:
         return JsonResponse(signup_student_serializer.errors,status=400)
 
-
 @csrf_exempt
 @api_view(['POST'])
 def signup_educator(request):
-    signup_educator_serializer = SignUpEducator_Serializer(data=request.data)
-
+    signup_educator_serializer =  serializers.SignUpEducator_Serializer(data=request.data)
     if signup_educator_serializer.is_valid():
         signup_educator_serializer.save()
         return JsonResponse(signup_educator_serializer.data, status=status.HTTP_201_CREATED)
@@ -48,7 +52,7 @@ def login_student(request):
     password = request.data['password']
 
     # check whether 'user' is an educator or student
-    user_type = User.objects.get(username=username)
+    user_type = models.User.objects.get(username=username)
     if user_type.educator:
         return JsonResponse({"message": "student access only"})
 
@@ -56,10 +60,10 @@ def login_student(request):
     headers = {'content-type': 'application/json'}
 
     # jwt library does authentication internally and then generates a token based on user credentials
-    obtain_jwt_token = requests.post(url='http://127.0.0.1:8000/api/token/', data=json.dumps(payload), headers=headers)
+    obtain_jwt_token = requests.post(url='/api/token/', data=json.dumps(payload), headers=headers)
     if obtain_jwt_token.status_code == 200:
         data = json.loads(obtain_jwt_token.text)
-        redis_token = user_utils.setex_jwt_token(token=data['access'],
+        redis_token = utils.setex_jwt_token(token=data['access'],
                                                  time=constants.REDIS_CONSTANTS['TIMEOUT'],
                                                  username=username,)
 
@@ -76,7 +80,7 @@ def login_educator(request):
     password = request.data['password']
 
     # check whether 'user' is an educator or student
-    user_type = User.objects.get(username=username)
+    user_type = models.User.objects.get(username=username)
     if user_type.student:
         return JsonResponse({"message": "educator access only"})
 
@@ -84,11 +88,11 @@ def login_educator(request):
     headers = {'content-type': 'application/json'}
 
     # jwt library does authentication internally and then generates a token based on user credentials
-    obtain_jwt_token = requests.post(url='http://127.0.0.1:8000/api/token/', data=json.dumps(payload), headers=headers)
+    obtain_jwt_token = requests.post(url='/api/token/', data=json.dumps(payload), headers=headers)
     if obtain_jwt_token.status_code == 200:
         data = json.loads(obtain_jwt_token.text)
 
-        redis_token = user_utils.setex_jwt_token(token=data['access'],
+        redis_token = utils.setex_jwt_token(token=data['access'],
                                                  time=constants.REDIS_CONSTANTS['TIMEOUT'],
                                                  username=username,)
 
@@ -111,7 +115,7 @@ def new_access_token(request):
     payload = {'refresh': request.data['refresh']}
     headers = {'content-type': 'application/json'}
 
-    access_token = requests.post(url='http://127.0.0.1:8000/api/token/refresh/',
+    access_token = requests.post(url='/api/token/refresh/',
                                      data=json.dumps(payload), headers=headers)
 
     if access_token.status_code == 200:
