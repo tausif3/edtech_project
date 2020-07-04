965879fce010de4cf82008458eb141c0770ade9a
Anupam Nirwan <anupam.nirwan@magicpin.in>
2020-07-04 23:52:17 +0530

    Add Template and Static Path to Settings

--- edtech_web/settings.py
+++ edtech_web/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'edtech_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'edtech_web/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -141,3 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "edtech_web/static"),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
