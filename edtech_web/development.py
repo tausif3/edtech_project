250967fb036c9e0eb09e810f43164627f9aaa160
Anupam Nirwan <anupam7nirwan@gmail.com>
2020-07-07 00:09:02 +0530

    Add Alternative Column Design

--- edtech_web/development.py
+++ edtech_web/development.py
@@ -2,7 +2,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
